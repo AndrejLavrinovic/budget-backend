d4b0f5dbaccc4bae9fdf877efcbb5e755c35efc3
andrej.lavrinovic <andrej.lavrinovic@smartbear.com>
2017-12-22 10:34:35 +0000

    esapi encoding

--- modules/budget/src/main/java/ie/altech/budget/Application.java
+++ modules/budget/src/main/java/ie/altech/budget/Application.java
@@ -1,11 +1,25 @@
 package ie.altech.budget;
 
+import org.owasp.esapi.ESAPI;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class Application {
+    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String... args){
+
+        // ESAPI encodeing check
+        String str = "Hello world!\n";
+        String clean = str.replace('\n', '_').replace('\r', '_');
+        String encoded = ESAPI.encoder().encodeForHTML(str);
+        LOGGER.info("Log output is here ===> " + str);
+        LOGGER.info("Log output is here ===> " + clean + " (cleaned)");
+        LOGGER.info("Log output is here ===> " + encoded + " (encoded)");
+
         SpringApplication.run(Application.class, args);
     }
 }
